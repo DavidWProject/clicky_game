49a731f27d5b908369220d7047f46c56350b4a5a
David Wong <davidwong@Davids-MacBook-Pro-4.local>
2018-11-08 13:54:52 -0500

    fixed text for reset

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,9 +7,9 @@ const Header = props => (
     <h2>
       How to Play: Click on an Image to start, but don't click on the same image twice!
     </h2>
-  <h1>
-        <a href="/" style={{ textDecoration: 'none' }}>Click here to reset the game and top scores</a>
-  </h1>
+  <h2>
+        <a href="/" style={{ textDecoration: 'none' }}>Click here to reset the game and high scores</a>
+  </h2>
   </header>
 );
 
