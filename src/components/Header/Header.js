370acebfc5c64e439daf6c22dbf7dc24d9c4629f
David Wong <davidwong@Davids-MacBook-Pro-4.local>
2018-11-08 13:08:17 -0500

    added funcitonality

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = props => (
+  <header className="header">
+    <h1>Clicky Game!</h1>
+    <h2>
+      How to Play: Click on an Image to start, but don't click on the same image twice!
+    </h2>
+  </header>
+);
+
+export default Header;
